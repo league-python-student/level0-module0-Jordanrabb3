65a0707305be17fc03fec375e0ff1876108b950f
root <nono@none.com>
2020-08-06 21:40:27 +0000

    Day4

---
+++ _03_print_and_popups/Day 4.py
@@ -0,0 +1,11 @@
+from tkinter import simpledialog, messagebox, Tk
+
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw(
+
+    answer = simpledialog, askstring(title = None, prompt='How old are you')
+
+    age = int(answer)
+
+    age = age + 1
--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -2,16 +2,16 @@ import turtle
 from PIL import Image
 
 class Eye():
-    def __init__(self, turtle=None, x=0, y=0, radius=30):
-        self.turtle = turtle
+    def __init__(self, myTurtle=None, x=0, y=0, radius=30):
+        self.myTurtle = myTurtle
         self.x = x
         self.y = y
         self.radius = radius
         
-        self.turtle.penup()
+        self.myTurtle.penup()
         
     def draw(self):
-        self.turtle.begin_fill()
+        self.myTurtle.begin_fill()
         self.myTurtle.goto(self.x, self.y)
         self.myTurtle.circle(radius=self.radius, steps=20)
         self.myTurtle.end_fill()
@@ -40,9 +40,13 @@ def keyPressed():
 
     # 10. Increment the x and y variables of the 2 eye variables by 5:
     #     leftEye.x += 5
-    
+    leftEye.x +=5
+    rightEye.x +=5
+    rightEye.y +=-5
+    leftEye.y +=5
     # 11. Call the .draw() method for both eye variables.
-
+    leftEye.draw()
+    rightEye.draw()
 if __name__ == '__main__':
     window = turtle.Screen()
     
@@ -52,19 +56,19 @@ if __name__ == '__main__':
     #    c. Rename the image something short (e.g. cat.gif)
     #    d. Save the image to your computer's desktop
     #    e. Drag and drop the image into your _04_amazing_games python package
-    
+    bgImage = 'bigEyedCat2.gif'
     # 2. Call the setBackground() function with your variable inside of the parenthesis
     #    for example, setBackground(bgImage)
-    
+    setBackground(bgImage)
     # 3. Make a new turtle
-    
+    turtle2 = turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-    
+    turtle2.color('pink')
     # 5. Set the turtle width to 0 so no outlines are drawn
-    
+    turtle2.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-    
+    turtle2.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your processing window. 
     #    Variables for x and y have been created at the top of your sketch, 
@@ -73,9 +77,11 @@ if __name__ == '__main__':
     # 8. After you've found the x and y for the eyes create 2 eye variables and initialize them:
     #    leftEye = Eye(turtle=myTurtle, x=-34, y=11, radius=30)  
     #    rightEye = Eye(turtle=myTurtle, x=40, y=-5, radius=30)
-    
+    leftEye = Eye(myTurtle=turtle2, x=-34, y=29.8)
+    rightEye = Eye(myTurtle=turtle2, x=40.0, y=9.0)
     # 9. Call the .draw() method on both eye variables
-
+    leftEye.draw()
+    rightEye.draw()
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screenClicked)
     window.onkeypress(keyPressed, 'space')
